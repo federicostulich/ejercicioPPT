2c3a828743a5d98e50198ad608bc78d7f6ce6754
Fs <fede.stulich@gmail.com>
2022-10-12 21:05:51 -0300

    TDD iteration

---
+++ src/Puntaje.js
@@ -0,0 +1,7 @@
+function Puntaje(){
+    this.resultado = function(){
+        return "0 a 0";
+    }
+}
+
+module.exports = Puntaje;
--- tests/juego.test.js
+++ tests/juego.test.js
@@ -1,4 +1,6 @@
+const Puntaje = require("../src/Puntaje");
+
 test("Puntaje 0 a 0", ()=>{
-    const puntaje = null;
+    const puntaje = new Puntaje();
     expect(puntaje.resultado()).toBe("0 a 0"); 
 })

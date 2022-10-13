fc2778ed2f1f94d60ef54481fd413a0edf83046c
Fs <fede.stulich@gmail.com>
2022-10-12 21:22:50 -0300

    TDD iteration

--- src/Puntaje.js
+++ src/Puntaje.js
@@ -2,6 +2,10 @@ function Puntaje(){
     this.resultado = function(){
         return "0 a 0";
     }
+
+    this.duelo() = function(){
+        this.resultado = "1 a 0";
+    }
 }
 
 module.exports = Puntaje;

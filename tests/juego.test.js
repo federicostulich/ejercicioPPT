48c74cfc2d46787e3b06038f7ca78cff2f4ff613
Fs <fede.stulich@gmail.com>
2022-10-12 21:58:34 -0300

    TDD iteration

--- tests/juego.test.js
+++ tests/juego.test.js
@@ -17,7 +17,10 @@ test("Puntaje 1 a 0", ()=>{
 
 test("Puntaje 1 a 1", ()=>{
     const puntaje = new Puntaje();
-    puntaje.duelo();
-    puntaje.duelo();
+    const papel = new Papel();
+    const piedra = new Piedra();
+    puntaje.duelo(papel,piedra);
+    puntaje.duelo(piedra,papel);
+    
     expect(puntaje.getResultado()).toBe("1 a 1"); 
 })
